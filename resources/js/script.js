20e2d90c8929c1a18eba85df6cc24d20d972f0dc
marcin-bojar <marcin.bojar@interia.pl>
2020-03-20 19:05:28 +0100

    Sticky nav animated

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,23 +1,31 @@
 $(document).ready(function() {
     var previousScroll = 0,
-        headerBottom = $('header').scrollTop() + ($('header').height() - 50),
-        contactTop = $('.section-contact').scrollTop();
-
+        headerBottom = $('header').scrollTop() + ($('header').height() - 50);
+       
     $(window).scroll(function() {
         var currentScroll = $(this).scrollTop();
         if(currentScroll > headerBottom) {
             if (currentScroll < previousScroll) {
                 $('.main-nav').addClass('sticky-nav');
                 $('.sticky-nav').removeClass('main-nav');
-            } else {
+                $('.sticky-nav').css("animation", "moveInUp .5s ease-out");
+                
+            } else {    
                 $('.sticky-nav').addClass('main-nav');
-             $('.main-nav').removeClass('sticky-nav'); 
+                $('.sticky-nav').css("animation", "none").css("animation", "moveOutUp .5s ease-out");
+                setTimeout(function() {
+                    $('.main-nav').removeClass('sticky-nav');
+                }, 500 );
             }
         } else {
-            $('.sticky-nav').addClass('main-nav');
-             $('.main-nav').removeClass('sticky-nav');  
+             $('.sticky-nav').css("animation", "moveOutUp .5s ease-out");
+             $('.sticky-nav').addClass('main-nav');
              $('.main-nav__list').css('animation', 'none');
-        }
+             setTimeout(function() {
+                $('.main-nav').removeClass('sticky-nav');
+            }, 500 );
+        }  
+             
 
         previousScroll = currentScroll;
     });
