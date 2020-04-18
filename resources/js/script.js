0a7a264c3cd3d20941d42fcd66081cde0d7cfd24
marcin-bojar <marcin.bojar@interia.pl>
2020-04-18 21:24:34 +0200

    Make it responsive p4

--- resources/js/script.js
+++ resources/js/script.js
@@ -6,19 +6,19 @@ $(document).ready(function() {
     // STICKY NAV
     $(window).scroll(function() {
         var currentScroll = $(this).scrollTop();
+       
         if(currentScroll > headerBottom) {
             if (currentScroll < previousScroll && width > 683) {
                 $('.main-nav').addClass('sticky-nav');
-                $('.sticky-nav').css("animation", "moveInUp .5s ease-out");
-                
-            } else {    
+                $('.sticky-nav').css("animation", "moveInUp .5s ease-out");    
+            } else {   
                 $('.sticky-nav').css("animation", "moveOutUp .5s ease-in");
                 setTimeout(function() {
                     $('.main-nav').removeClass('sticky-nav');
                 }, 450 );
             }
         } else {
-             $('.sticky-nav').css("animation", "moveOutUp .5s ease-in");    
+            $('.sticky-nav').css("animation", "moveOutUp .5s ease-in");    
             setTimeout(function() {
                 $('.main-nav').removeClass('sticky-nav');
                 $('.main-nav').css("animation", "none");
@@ -27,6 +27,24 @@ $(document).ready(function() {
         }  
 
         previousScroll = currentScroll;
+    });
+
+    // Mobile icon management
+    $(window).scroll(function() {
+        var currentScroll = $(this).scrollTop(),
+            contactTop = $('.section-contact').offset().top - 30;
+            console.log(contactTop, currentScroll);
+        if (currentScroll > headerBottom) {
+            $('.mobile-button__icon').css('fill', '#EE5A24');
+        } else {
+            $('.mobile-button__icon').css('fill', '#fff');
+        } 
+        if (currentScroll > contactTop) {
+            $('.mobile-button__icon').css('visibility', 'hidden'); 
+       } else {
+        $('.mobile-button__icon').css('visibility', 'visible');
+       } 
+
     });
 
 
@@ -38,6 +56,7 @@ $(document).ready(function() {
     $(window).resize(function() {
         if ($(window).width() <= 683) {
             $('.js--main-nav').css('display', 'none');
+            $('.mobile-button__icon').css('animation', 'none');   
             $('.mobile-button__icon').css('display', 'block');
         } else {
             $('.js--main-nav').css('display', 'block');
@@ -48,11 +67,13 @@ $(document).ready(function() {
 
 
     $('.mobile-button__icon').click(function() {
-            $('.mobile-nav').slideToggle(200);    
+            $('.mobile-nav').slideToggle(200);
+            $('.mobile-button__icon').css('fill', '#EE5A24');    
     });
 
     $('.mobile-nav__link').click(function() {
-        $('.mobile-nav').slideToggle(200);    
+        $('.mobile-nav').slideToggle(200);
+
 });
 
     // IMAGE POPUP
